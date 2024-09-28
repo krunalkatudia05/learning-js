24c0a2666e8163ce0f0c3fa1d5b9499d2ef1ee8b
krunalkatudia05 <kkatudia@gmail.com>
2024-09-28 16:57:14 +0000

    Arrays in Js

---
+++ basics/Array.js
@@ -0,0 +1,25 @@
+const myArr=[1,2,3,4,5,6]
+
+//myArr.push(19);
+//console.log(myArr);
+
+//myArr.pop();
+//console.log(myArr);
+
+//myArr.unshift(20);
+//console.log(myArr);
+
+//myArr.shift();
+//console.log(myArr);
+
+//console.log(myArr.join());
+//console.log(myArr)
+ //console.log(myArr.includes(3));
+//  console.log(myArr.indexOf(1));
+
+
+ console.log(myArr)
+ console.log(myArr.slice(1,4))
+console.log(myArr)
+//console.log(myArr.splice(1,3))
+//console.log(myArr)
