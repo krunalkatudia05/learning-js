c89eeb915959f426b71f58f02ded6cf8e9e10846
krunalkatudia05 <kkatudia@gmail.com>
2024-09-28 15:50:39 +0000

    Data Format in JS

--- basics/test.js
+++ basics/test.js
@@ -1,2 +1,12 @@
-console.log("krunal");
-console.log("hi once again")
+// console.log("krunal");
+// console.log("hi once again")
+
+//Date Format in JavaScript
+//let myDate = new Date()
+ //console.log(myDate.toString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleString());
+//  console.log(typeof myDate);
+ //console.log(myDate.getDay());
+
+ //Date Format in JavaScript END
