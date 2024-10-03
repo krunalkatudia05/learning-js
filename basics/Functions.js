58cba33dcf6419e1774834b7a34b6af8173cd26f
krunalkatudia05 <kkatudia@gmail.com>
2024-10-03 17:38:25 +0000

    Functions in JS commit 2

--- basics/Functions.js
+++ basics/Functions.js
@@ -53,3 +53,51 @@ console.log(result);
 */
 
 
+
+//Rest Operator in Functions.
+//Rest Operator is represented by ...(3 dots) in function parameter.
+//Rest Operator can be useful when we are not sure about the numbers of input parameter that we will be receiving.
+//It returns array object.
+/*
+function RestEx(...nums)
+{
+    return nums;
+}
+console.log(RestEx(1,2,3,4));
+*/
+
+//OutPut : [ 1, 2, 3, 4 ]
+
+
+
+/*
+here we took "val1" paramter and "...num" which is an rest operator. 
+*/
+function CalculateAgain(val1,...num)
+{
+    return num;
+}
+//here we will get out "2,3,4,5,6" because "1" will be assigned to "val1" parameter
+//and rest of the inputs will be assigned to rest operator
+//console.log(CalculateAgain(1,2,3,4,5,6));
+//OutPut : [ 2, 3, 4, 5, 6 ]
+
+
+
+//Function with Object
+
+// const user={
+//     username:"David Shah",
+//     price:100
+// }
+//We can name anything we want as an parameter name of the function that can be used as an reference of the object that we 
+//will be passing at the time of calling the function.
+// function DisplayUser(anyobject)
+// {
+//     return `Username is ${anyobject.username} and price is ${anyobject.price}`
+// }
+
+//console.log(DisplayUser(user));
+//OutPut : Username is David Shah and price is 100
+
+
