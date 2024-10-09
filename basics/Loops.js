5f1389d24d6649ca3955bbaeaf42248bc745a3c1
krunalkatudia05 <kkatudia@gmail.com>
2024-10-09 19:02:33 +0000

    Loops & Array in Advanced

--- basics/Loops.js
+++ basics/Loops.js
@@ -1,7 +1,12 @@
 //Loops and it's Types
+/*
+------------------------------- Important Notes -----------------------------
+1.) Any of the loop "for of" , "for in" , "for each" does not return the when we use arrayname.foreach method.
+*/ 
 
 
-//for of
+
+//-------------------------------------------for of loop in Javascript--------------------------------------------------------
 /*
 While using "for of" loop while iterating loop we get directly value of the "element" when we log the 'element'.
 */ 
@@ -66,8 +71,105 @@ map.set('name','David Shah');
 // }
 
 
+//------------------------------------------- for in loop in Javascript --------------------------------------------------------
+
+/*
+Basic difference between "for of" and "for in" loop is that
+1.) "for in" can iterate through object  And "for of" loop cannot loop Object
+2.) In "for in" when we iterate it provides the Index while Iterating but In "for of" when we iterate it provides the value directly.
+
+*/ 
+//Iterating Object using "for in"
+// const myObj=
+// {
+//     "id":123,
+//     'name' : 'David Shah',
+//     'designation' : 'Software Devloper'   
+// }
+
+// for (const element in myObj) {
+//  console.log(myObj[element]);
+// }
+
+
+//Iterating loop using "for in"
+// const myArrIn=[1,2,3,4,5,6]
+// for (const element in myArrIn) {
+//     console.log(myArrIn[element]);
+// }
+
+
+//Iterating loop using Map
+// const map=new Map()
+// map.set('id',12345);
+// map.set('name','David Shah');
+// map.set('designation','Software Devloper');
+// map.set('name','David Shah');
+
+//below code is does not log anything because "map" is not iterable.
+// for (const [element,eleval] in map) {
+//     console.log(element+' '+eleval);
+// }
+
+
+//------------------------------------------- forEach loop in Javascript --------------------------------------------------------
+
+// while using "programminglang.forEach" loop we have to provide an unnamed function (function without name).
+
+//forEach function with callback function.
+// const programminglang=["c#",".net core","Java","SQL"]
+// const getValofprogramminglang = programminglang.forEach( (element) => {
+//     console.log(element);    
+//     //return element;
+// });
+
+//whenever we store the value of the forEach loop it does not return anyvalue hence it will return undefined. 
+// Even if we write "return element"(line no 123) of the function then too it will return undefined.
+//OutPut : return value of getValofprogramminglang : undefined
+//console.log(`return value of getValofprogramminglang : ${getValofprogramminglang}`);
+
+
+// const programminglang=["c#",".net core","Java","SQL"]
+// const getValofprogramminglang = programminglang.forEach(function (element){
+//     console.log(element);    
+// });
+
+//console.log(`return value of getValofprogramminglang : ${getValofprogramminglang}`);
 
 
 
+//forEach function with external function
+
+// function printme(element)
+// {
+//     console.log(element)
+// }
+// const programminglang=["c#",".net core","Java","SQL"]
+// //Note : while passing function in forEach we have just provided reference(printme) of the function and we did not executed
+// //the function by providing "()" brackets.
+// const getValofprogramminglang = programminglang.forEach(printme);
 
 
+//Paramters in ForEach Loop
+// const programminglang=["c#",".net core","Java","SQL"]
+// programminglang.forEach( (value,key,arraylst)=>{
+//     //console.log(arraylst); //this line prints the array list
+//     //console.log(key); //this line prints the key of the array list
+//     //console.log(value); //this line prints the value of the array list
+// });
+
+//Iterating Array Of Object in Loop
+//  const programminglang = [
+//     {
+//         "programming":"JavaScript",
+//         "extenstion":"js"
+//     },
+//     {
+//         "programming":"C#",
+//         "extenstion":"cs"
+//     }
+//  ]
+// programminglang.forEach( (value) => {    
+//     console.log(value.programming); //this line prints the programming language name
+//     console.log(value.extenstion); //this line prints the programming language name
+// });
